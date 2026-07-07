7569e668e6eda456b80f6ef266fe124d63c7948d
codsach <codsach@gmail.com>
2026-07-07 13:33:05 +0530

    removed test files

--- scratch/check_errors.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const output = execSync('npx tsc src/app/(dashboard)/investigator/submit/page.tsx --noEmit --esModuleInterop --skipLibCheck --jsx react-jsx --moduleResolution bundler --target esnext --module esnext --baseUrl . --paths "@/*": ["src/*"]', { encoding: 'utf8' });
-  console.log(output);
-} catch (error) {
-  console.log(error.stdout);
-  console.log(error.stderr);
-}
